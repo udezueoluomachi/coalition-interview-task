1d6ea9067c9c1142aa9edaeb92d4827a18dccbf9
udezueoluomachi <74429592+udezueoluomachi@users.noreply.github.com>
2023-12-28 00:02:40 +0100

    making progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,59 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+    <div className="w-full h-full bg-section-1 bg-no-repeat bg-cover bg-left-top">
+      <header className="w-full h-12 md:h-16 px-4 lg:px-40 flex items-center justify-between text-white">
+        <div className='w-[30px] h-[30px] md:w-[40px] md:h-[40px] bg-Logo bg-no-repeat bg-center bg-cover'></div>
+        <div className='hidden lg:block'>
+          <a href="#history" className='text-base mx-2 md:mx-6 hover:underline'>01. HISTORY</a>
+          <a href="#team" className='text-base mx-2 md:mx-6 hover:underline'>02. TEAM</a>
+        </div>
+      </header>
+      <div className="w-full text-center text-5xl md:text-9xl font-normal mt-32">
+        <p className="text-gray-800">
+          LOSANGELES
+        </p>
+        <p className="text-slate-700">
+          MOUNTAINS
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
+    <div id="history" className="w-full h-full max-h-max bg-peak bg-no-repeat bg-cover bg-top bg-scroll">
+      <header className="sticky top-0 bg-white w-full h-12 md:h-16 px-4 lg:px-40 flex items-center justify-between text-slate-700">
+        <div className="flex w-max">
+          <div>
+            <div className='mx-1 rounded-full border border-slate-700 w-[30px] h-[30px] md:w-[40px] md:h-[40px] bg-Logo bg-no-repeat bg-center bg-cover'></div>
+          </div>
+          <div className="mx-1 w-full text-center text-md font-bold">
+            <p className="text-gray-800">
+              LOSANGELES
+            </p>
+            <p className="text-slate-700 mt-[-8px]">
+              MOUNTAINS
+            </p>
+          </div>
+        </div>
+        <div className='hidden lg:block'>
+          <a href="#history" className='text-base mx-2 md:mx-6 underline hover:no-underline'>01. HISTORY</a>
+          <a href="#team" className='text-base mx-2 md:mx-6 underline hover:no-underline'>02. TEAM</a>
+        </div>
+      </header>
+      <div className="px-4 lg:px-40">
+        <div className="flex items-center mt-10">
+          <p className="text-slate-700/55 text-5xl md:text-9xl font-semibold">01</p>
+          <div className='md:translate-y-8'>
+            <h2 className="font-bold text-slate-700 text-lg">HISTORY</h2>
+            <div className="h-[10px] w-[10px] bg-slate-700/55"></div>
+          </div>
+        </div>
+        <p className="ml-8 md:ml-20 text-gray">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae ducimus quis eum nihil explicabo doloremque perspiciatis commodi ea rem mollitia, fugiat temporibus illo sint voluptatum veniam atque itaque tempora? Possimus velit libero, commodi ipsam voluptatem corrupti, blanditiis aperiam fuga incidunt inventore consequuntur voluptates rem. Quod totam eos omnis cupiditate et.
+        </p>
+      </div>
+    </div>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ export default {
     content: ["./src/**/*.{js,ts,jsx,tsx}","./index.html"],
     theme: {
       extend: {
+        backgroundImage : {
+          "section-1" : "url('/section-1.png')",
+          "peak" : "url('/peak.png')",
+          "last-section-tab-1" : "url('/last-section-tab-1.png')",
+          "last-section-tab-2" : "url('/last-section-tab-2.png')",
+          "carousel-1" : "url('/carousel-1.png')",
+          "carousel-2" : "url('/carousel-2.png')",
+          "Logo" : "url('/Logo.png')",
+        }
       },
     },
     plugins: [],
